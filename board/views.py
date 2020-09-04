390de5bcab63bb59f8dc8fd28eec69c1670a0b56
Ben <bmdvpanga@gmail.com>
2020-09-04 18:16:23 -0400

    UI renders the solution based on entered board

--- board/views.py
+++ board/views.py
@@ -6,15 +6,15 @@ from .utils import SudokuSolver
 def index(request):
     board = {
       'board':  [
-            [0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0]
+            [5,3,0,0,7,0,0,0,0],
+            [6,0,0,1,9,5,0,0,0],
+            [0,9,8,0,0,0,0,6,0],
+            [8,0,0,0,6,0,0,0,3],
+            [4,0,0,8,0,3,0,0,1],
+            [7,0,0,0,2,0,0,0,6],
+            [0,6,0,0,0,0,2,8,0],
+            [0,0,0,4,1,9,0,0,5],
+            [0,0,0,0,8,0,0,7,9]
         ],
         'range': range(9)
     }
@@ -24,10 +24,9 @@ def index(request):
 
     if(request.method == 'POST'):
         solver = SudokuSolver(request)
-        
-        print(solver.board)
 
-        solver.solve_board( solver.board )
+        solved = solver.solve_board( solver.board )
+        board["board"] = solved[1].tolist()
 
         return render(request, 'board/index.html', board)
 
